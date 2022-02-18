748c9fddcd08859bf18bdf42ecee7d869117f850
Arfan A. Khan <73901113+ArfanGits@users.noreply.github.com>
2022-02-18 20:22:09 +0600

    Category page frontend mastering done

---
+++ resources/views/frontend/pages/category/category.blade.php
@@ -0,0 +1,5 @@
+@extends('frontend.layouts.app')
+@section('content')
+    @include('frontend.pages.category.sections.breadcrumb')
+    @include('frontend.pages.category.sections.body-content')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::get('/','Frontend\HomeController@index')->name('home');
 Route::get('404','Frontend\ErrorController@index')->name('404');
 Route::get('blog','Frontend\BlogController@index')->name('blog');
 Route::get('blog-details','Frontend\BlogDetailController@index')->name('blog-details');
+Route::get('category','Frontend\CategoryController@index')->name('category');
